69ee0e0b80046240218ae2cb6c3a3faa153cb642
dubemoyibe-star <dubemoyibe@gmail.com>
2026-09-22 18:04:47 +0100

    remooved unused imports in the index.ts of the database

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -4,13 +4,7 @@ export {
   DEFAULT_CONNECTION_TIMEOUT_MS,
   type CreatePrismaClientOptions,
 } from "./client";
-export {
-  PrismaClient,
-  type Account,
-  type Session,
-  type User,
-  type Verification,
-} from "./generated/prisma/client";
+export { PrismaClient, type User } from "./generated/prisma/client";
 export {
   assertDatabaseEnv,
   checkDatabaseEnv,
